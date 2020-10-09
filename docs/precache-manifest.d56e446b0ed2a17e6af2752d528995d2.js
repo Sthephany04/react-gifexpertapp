22213a2ca27c184b979494ee417f9d4bb99737ae
Sthephany Granados <lsgranados04@gmail.com>
2020-10-08 19:08:59 -0500

    docs agregado

---
+++ docs/precache-manifest.d56e446b0ed2a17e6af2752d528995d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20c32af38512bb01be40be8c7554d7ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be54548a388716f61fab",
+    "url": "/static/css/main.b564d703.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be54548a388716f61fab",
+    "url": "/static/js/main.00aade50.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
